63cc02749b222a393319daff934b4f03557a1563
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-09-19 10:06:56 +0100

    Create quote.c

---
+++ c-introduction/quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("C programming lanuage was developed by Dennis Ritchie\n");
+    printf("in the year 1972.\n");
+    printf("Over the years it has become vital in the creation of many of the software/systems we use today.");
+    return 0;
+}
