d9868841529f47b6582aa52093e0eae5725e3af1
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-09-20 19:56:16 +0100

    Update hello.c

--- c-variables-data_types/hello.c
+++ c-variables-data_types/hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  printf("Hello, C Programming!");
-  // This prints out the text Hello, C Programming as output.
+  printf("Hello, C Programming!"); // This prints out the text 'Hello, C Programming!' as output.
+  
   /*C is still of relevance today, seeing as its been used to develop many of 
-  the software systems we use in our daily lives*/
+  the software systems we use in our daily lives.
+  It is very flexible and easy to understand as well.*/
   return 0;
 }
