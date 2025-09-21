e2d77ad5a0d40baf0a6f001961ea66d90e379925
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-09-21 06:29:42 +0100

    Create tokens_demo.c

---
+++ c-variables-data_types/tokens_demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int level = 300;   // int => keyword, i => identifier, = => operator, 35 => literal, ; => punctuation
+    float cgpa = 4.72; // float => keyword, cgpa => identifier, = => operator, 4.72 => literal, ; => punctuation
+    char grade = 'A';  // char =. keyword, age => identifier, = => operator, 20 => literal, ; => punctuation
+
+    printf("Integer: %d\n", level);
+    printf("Float: %f\n", cgpa);
+    printf("Char: %c", grade);
+
+    return 0;
+}
+
