dea95b3a69f638cfaa9ecb53a7a511a3f977473b
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-09-21 06:58:12 +0100

    Create variables_demo.c

---
+++ c-variables-data_types/variables_demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 13;
+    float height = 4.9;
+    char grade = 'B';
+
+    printf("Age: %d", age);
+    printf("Height: %f", height);
+    printf("Grade: %c", grade);
+}
