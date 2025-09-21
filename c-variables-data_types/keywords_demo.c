7b118f504c5d809fd4abdfc94c0e9f94bcb1ff1a
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-09-21 06:44:30 +0100

    Create keywords_demo.c

---
+++ c-variables-data_types/keywords_demo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int test = 5;
+    float mul = 2.5;
+
+    if (test == 5)
+    {
+        printf("Keywords are powerful");
+    }
+
+    else
+    {
+        printf(" ");
+    }
+
+    return 0;
+}
