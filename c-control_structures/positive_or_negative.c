b330c968e0f0415dd4415ce7624ed30a18aff761
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-21 17:53:50 +0100

    Update positive_or_negative.c

--- c-control_structures/positive_or_negative.c
+++ c-control_structures/positive_or_negative.c
@@ -9,17 +9,17 @@ int main()
 
     if (num > 0)
     {
-        printf("%d is a positive number.", num);
+        printf("%d is a positive number.\n", num);
     }
 
     else if (num < 0)
     {
-        printf("%d is a negative number.", num);
+        printf("%d is a negative number.\n", num);
     }
 
     else
     {
-        printf("%d is zero.", num);
+        printf("%d is zero.\n", num);
     }
 
     return 0;
