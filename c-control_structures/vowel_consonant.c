0b3810a57ab28fc9dbb2490291b45780f2cb08f2
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-22 21:04:54 +0100

    Create vowel_consonant.c

---
+++ c-control_structures/vowel_consonant.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h> 
+
+int main() {
+    char c;
+    char lower_c;
+
+    printf("Enter an alphabet: ");
+    scanf(" %c", &c); 
+
+    lower_c = tolower(c);
+
+    if (lower_c >= 'a' && lower_c <= 'z') {
+        if (lower_c == 'a' || lower_c == 'e' || lower_c == 'i' || lower_c == 'o' || lower_c == 'u') {
+            printf("%c is a Vowel.\n", c);
+        } else {
+            printf("%c is a Consonant.\n", c);
+        }
+        
+    } else {
+        printf("Error: The input '%c' is not an alphabet.\n", c);
+    }
+
+    return 0;
+}
