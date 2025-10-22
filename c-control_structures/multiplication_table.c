ecec5477b6d4a72977ad31b0d10feac2903238b4
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-22 07:05:50 +0100

    Update multiplication_table.c

--- c-control_structures/multiplication_table.c
+++ c-control_structures/multiplication_table.c
@@ -9,19 +9,11 @@ int main() {
     printf("Enter an integer (n): ");
     scanf("%d", &n);
 
+    printf("Multiplication table of %d\n", n);
+    for (i = 1; i <= 10; i++) {
+    int result = n * i;
 
-    if (n < 1) {
-        printf("Invalid input. Please enter a valid integer.\n");
+    printf("%d x %d = %d\n", n, i, result);
     }
-
-    else {
-        printf("Multiplication table of %d\n", n);
-        for (i = 1; i <= 10; i++) {
-        int result = n * i;
-
-        printf("%d x %d = %d\n", n, i, result);
-    }
-    }
-
     return 0;
 }
