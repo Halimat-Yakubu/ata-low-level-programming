ce7bd9411723467e747312755f9a25d14caf1cc2
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-22 21:38:36 +0100

    Create prime_number_checker.c

---
+++ c-control_structures/prime_number_checker.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    int i;
+    int is_prime = 1;
+
+    printf("Enter a positive integer: ");
+
+    if (scanf("%d", &number) != 1 || number <= 0) {
+        printf("Invalid input. Please enter a positive integer greater than 0.\n");
+        return 1;
+    }
+
+    if (number <= 1) {
+        is_prime = 0;
+    } else {
+        for (i = 2; i < number; i++) {
+            if (number % i == 0) {
+                is_prime = 0;
+                
+                break;
+            }
+        }
+    }
+
+  
+    if (is_prime) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is NOT a prime number.\n", number);
+    }
+
+    return 0;
+}
