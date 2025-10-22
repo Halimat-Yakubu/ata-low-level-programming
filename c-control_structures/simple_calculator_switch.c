f5921aa576d7815222e871a5c68251522a274f11
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-22 07:32:33 +0100

    Update simple_calculator_switch.c

--- c-control_structures/simple_calculator_switch.c
+++ c-control_structures/simple_calculator_switch.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
 
 int main() {
+    double num1;
+    double num2;
     char op;
-    double num1, num2, result;
+    double result;
 
-    printf("--- Simple Calculator ---\n");
-    printf("Enter an expression (e.g., 10 + 5 or 20.5 * 3): ");
+    printf("Enter two numbers: \n");
+    scanf("%lf %lf", &num1 &num2);
+    
+    printf("Enter operator: (+, -, *, /) \n");
+    scanf("%c", &op)
 
-    if (scanf("%lf %c %lf", &num1, &op, &num2) != 3) {
-        printf("Error: Invalid input format. Please enter two numbers and one operator (e.g., 10 + 5).\n");
-        return 1;
-    }
 
     switch (op) {
         case '+':
             result = num1 + num2;
-            printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
+            printf("%lf %c %lf = %lf\n", num1, op, num2, result);
             break;
         case '-':
             result = num1 - num2;
-            printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
+            printf("%lf %c %lf = %lf\n", num1, op, num2, result);
+            break;
+        case '+':
+            result = num1 + num2;
+            printf("%lf %c %lf = %lf\n", num1, op, num2, result);
             break;
         case '*':
             result = num1 * num2;
-            printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
+            printf("%lf %c %lf = %lf\n", num1, op, num2, result);
             break;
         case '/':
             if (num2 == 0) {
                 printf("Error: Division by zero is not allowed.\n");
-                return 1;
-            }
-            result = num1 / num2;
-            printf("%.2lf %c %.2lf = %.2lf\n", num1, op, num2, result);
-            break;
+            } else {
+                 result = num1 / num2;
+                 printf("%lf %c %lf = %lf\n", num1, op, num2, result);
+               }
+                break;
         default:
             printf("Error: Invalid operator '%c'. Supported operators are +, -, *, /.\n", op);
             return 1;
