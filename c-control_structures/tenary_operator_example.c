66c1d149f0e297371a807c50529b1d6c666419b3
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-22 21:21:06 +0100

    Create tenary_operator_example.c

---
+++ c-control_structures/tenary_operator_example.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number;
+    const char* result_message;
+
+    printf("Enter an integer: ");
+
+    if (scanf("%d", &number) != 1) {
+        printf("Invalid input. Please enter a valid integer.\n");
+        return 1;
+    }
+
+    result_message = (number > 0) ? "Positive" : "Non-positive";
+    printf("%d is %s\n", number, result_message);
+
+    return 0;
+}
