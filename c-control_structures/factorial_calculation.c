38c9aab2b5959638b93a581f3e1ccb9a4444f95d
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-24 11:28:55 +0100

    Create factorial_calculation.c

---
+++ c-control_structures/factorial_calculation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    long long factorial = 1; 
+    int i; 
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Error: Factorial is not defined for negative numbers.\n");
+    } 
+
+
+    i = num; 
+    while (i > 0) {
+        factorial = factorial * i; 
+        i--; 
+    }
+
+    printf("The factorial of %d is %lld.\n", num, factorial);
+
+    return 0;
+}
