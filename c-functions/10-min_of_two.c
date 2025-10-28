1233f46bced6d4ba9dd3deea444edb98e928acb9
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 06:57:38 +0100

    Create 10-min_of_two.c

---
+++ c-functions/10-min_of_two.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int find_min(int a, int b);
+
+int main() {
+    int num1, num2, max;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    min = find_min(num1, num2);
+
+    printf("Minimum number is: %d\n", max);
+
+    return 0;
+}
+
+int find_min(int a, int b) {
+    if (a <  b)
+        return a;
+    else
+        return b;
+}
