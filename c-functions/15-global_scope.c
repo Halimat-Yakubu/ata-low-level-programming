02c8835fb82faf86edf01cae1979e07fc1450e8d
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 07:08:45 +0100

    Create 15-global_scope.c

---
+++ c-functions/15-global_scope.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int global_count = 0;
+
+void increment_global_count();
+
+int main() {
+    increment_global_count();
+    printf("Global count after 1st increment: %d\n", global_count);
+
+    increment_global_count();
+    printf("Global count after 2nd increment: %d\n", global_count);
+
+    increment_global_count();
+    printf("Global count after 3rd increment: %d\n", global_count);
+
+    return 0;
+}
