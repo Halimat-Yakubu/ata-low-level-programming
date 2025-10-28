7458d29abf16b931d0da99ab7fe4dd39d9aa09d4
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 06:52:07 +0100

    Create 4-get_square_of_five.c

---
+++ c-functions/4-get_square_of_five.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>  
+
+int get_square_of_five();
+
+int main() {
+    int result;
+
+    result = get_square_of_five();
+
+    printf("Square of 5 is: %d\n", result);
+
+    return 0;
+}
+
+
+int get_square_of_five() {
+    double square = pow(5, 2); 
+    return (int)square;         
+ }
+
