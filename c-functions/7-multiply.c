35688d179e0e02934ed79e11d5b884fe8637547c
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 06:55:11 +0100

    Create 7-multiply.c

---
+++ c-functions/7-multiply.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int multiply (int a, int b);
+
+int main() {
+    int num1, num2, result;
+
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+
+    result = multiply (num1, num2);
+
+    printf("Result of multiplication: %d\n, result);
+
+    return 0;
+}
+
+
+int multiply (int a, int b) {
+    return a * b;
+}
