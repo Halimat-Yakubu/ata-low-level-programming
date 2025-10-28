c675733af2c8c93089660bdcb9dba222ad7f05ef
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 06:49:35 +0100

    Update 1-print_integer.c

--- c-functions/1-print_integer.c
+++ c-functions/1-print_integer.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+void print_number(int num);
+
+int main() {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    print_number(number);
+
+    return 0;
+}
+
+void print_number(int num) {
+    printf("The number you is: %d\n", num);
+}
 
