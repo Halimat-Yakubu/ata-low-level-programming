90d39186d78cdf069c5fde0e04a8c89da9f56e0f
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 07:16:53 +0100

    Update 9-max_of_two.c

--- c-functions/9-max_of_two.c
+++ c-functions/9-max_of_two.c
@@ -6,10 +6,10 @@ int find_max(int a, int b);
 int main() {
     int num1, num2, max;
 
-    printf("Enter first number: ");
+    printf("Enter first number: \n");
     scanf("%d", &num1);
 
-    printf("Enter second number: ");
+    printf("Enter second number: \n");
     scanf("%d", &num2);
 
     max = find_max(num1, num2);
