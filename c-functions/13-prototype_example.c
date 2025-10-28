2f8d1789564072ab8d152fea5a35ecfba4d4da1e
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-10-28 07:05:43 +0100

    Create 13-prototype_example.c

---
+++ c-functions/13-prototype_example.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+void greet_user(char *name);
+
+int main() {
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    greet_user(name);
+
+    return 0;
+}
+
+void greet_user(char *name) {
+    printf("Hello, %s! Welcome to the program.\n", name);
+}
