b8a8d18ca726ae5a1bf2435639290d27a5b3aef7
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-12-09 19:11:56 +0100

    Update hello.c

--- c-variables-data_types/hello.c
+++ c-variables-data_types/hello.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - the main function
- * description: The entry point of our C program
- * return: 0
-*/
 int main() {
   printf("Hello, C Programming!"); // This prints out the text 'Hello, C Programming!' as output.
   
