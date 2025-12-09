593fc1ce616a5023f6ffb69306067c8c220c953a
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-12-09 14:56:36 +0100

    Update keywords_demo.c

--- c-variables-data_types/keywords_demo.c
+++ c-variables-data_types/keywords_demo.c
@@ -2,25 +2,13 @@
 
 int main()
 {
-    int num;
+    int num = 5;
     char option;
     float extra;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    
+
     if (num == 5) {
-        option = 'A';
-        printf("%c. You got the special number!", option);
+        printf("Keywords are powerful");
     }
     
-    else if(num < 5){
-        option = 'B';
-        printf("%c. Too low, try again.", option);
-    }
-    
-    else {
-        option = 'C';
-        printf("%c. Too high, try again.", option);
-    }
     return 0;
 }
