d658386d3b455886d7a2d31c0b6cc141f213e403
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-12-09 15:05:52 +0100

    Update variables_demo.c

--- c-variables-data_types/variables_demo.c
+++ c-variables-data_types/variables_demo.c
@@ -6,6 +6,6 @@ int main()
     float height = 5.9;
     char grade = 'A';
     
-    printf ("Age: %d, Height: %f, Grade: %c", age, height, grade);
+    printf ("Age: %d, Height: %.1f, Grade: %c", age, height, grade);
     return 0;
 }
